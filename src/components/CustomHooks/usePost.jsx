af41a9a53ca42fe665bfc0fa0ae4e2a45061a07a
Md Naseer uddin <umdnaseer17@gmail.com>
2025-04-30 00:27:56 +0530

    LOGIN and LOGOUT of the tutor

--- src/components/CustomHooks/usePost.jsx
+++ src/components/CustomHooks/usePost.jsx
@@ -1,25 +1,64 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { notifyError } from "../admin/toastConfig";
 
-const usePost = (url, payload) => {
-  const [response, setResponse] = useState([]);
+const usePost = () => {
+  const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (!url || !payload) return;
-    
+  const post = async (url, payload) => {
     setLoading(true);
-    axios.post(url, payload)
-      .then((res) => setResponse(res.data))
-      .catch((err) => {
-        console.log(err);
-        notifyError(err.response?.data?.message || "Something went wrong");
-      })
-      .finally(() => setLoading(false));
-  }, [url, payload]);
+    try {
+      const res = await axios.post(url, payload);
+      setResponse(res.data);
+      return {data:res.data}
+    } catch (err) {
+      console.log(err);
+      notifyError(err.response?.data?.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return { response, loading };
-}
+  return { post, response, loading };
+};
 
 export default usePost;
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { notifyError } from "../admin/toastConfig";
+
+// const usePost = (url, payload) => {
+//   const [response, setResponse] = useState([]);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (!url || !payload) return;
+    
+//     setLoading(true);
+//     axios.post(url, payload)
+//       .then((res) => setResponse(res.data))
+//       .catch((err) => {
+//         console.log(err);
+//         notifyError(err.response?.data?.message || "Something went wrong");
+//       })
+//       .finally(() => setLoading(false));
+//   }, [url, payload]);
+
+//   return { response, loading };
+// }
+
+// export default usePost;
+
+
+
+
